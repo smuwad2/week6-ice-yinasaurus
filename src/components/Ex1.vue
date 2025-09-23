d0355cbb817d2451d958f853c8981fc622c5af3d
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-09-23 05:55:02 +0000

    ex1 done

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,6 +2,17 @@
     export default { 
         // Add Code Here
         // challenge: use array  
+        data(){
+            return {
+                message: "choose your favorite fruit:",
+                fruit1: "apple",
+                fruit2: "orange",
+                fruit3: "peach",
+
+                fruits:["apple","orange","peach"]
+                
+            }
+        }
     }
 </script>
 
@@ -12,6 +23,11 @@
         {{ fruit2 }} <input type="radio" name="fruit">
         {{ fruit3 }} <input type="radio" name="fruit">
     </div>
+    <h3>using array</h3>
+    {{ message }}</br>
+    <span v-for="fruit in fruits">
+        {{fruit}} <input type="radio" name="fruit"></input>
+    </span>
 </template>
 
 <style scoped>
