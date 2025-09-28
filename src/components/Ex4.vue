5b66568f67fdc2928ac3fcbf85e74db0f4186bbd
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-09-28 11:29:44 +0000

    ex4 done

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,23 +3,31 @@
         data() {
             return {
                 // Add code here
+                id:"demo1",
+                blueBox:true,
+                blueBtn:true,
             }
         },
         methods: { 
    			// Add code here
-		}
+            changeColour(){
+                // Toggle both box and button
+            this.blueBox= !this.blueBox;
+            this.blueBtn = !this.blueBtn;
+        }
     }
+}
 </script>
 
 <template>
     
     <!-- Modify code here -->
     <div class="container">
-        <div v-bind:id="id" class="m-2">
+        <div v-bind:id="id" class="m-2" :class="{'blueBox':blueBox,'redBox':!blueBox}">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <button type="button" :class="{'btn btn-primary':blueBtn, 'btn btn-danger':!blueBtn}" @click="changeColour" >Change Color</button>
     </div>
 
 </template>
